df41b7243667e6d60ca346cadfa409316370cd8f
gihyuncho <gihyuncho@backpac.kr>
2024-05-17 18:00:17 +0900

    wwdc vtt 파일 가공

---
+++ WWDC/Utilities/concatenate.py
@@ -0,0 +1,39 @@
+input_vtt = '225.vtt'
+output_vtt = '255con.vtt'
+
+f = open(input_vtt, "r")
+string = f.read()
+f.close()
+a = string.split("\n\n")
+
+new = ""
+time = ""
+tmp = ""
+for ele in a:
+    b = ele.split("\n")
+    c = b[0].split(" --> ")
+    if len(b) == 1:
+        new += (ele + "\n\n")
+        continue
+
+    chunk = b[1].strip()
+    if chunk[-1] == '.':
+        if time == "":
+            new += (ele + "\n\n")
+        else:
+            time += " --> " + c[1]
+            tmp += chunk
+            new += (time + "\n" + tmp + "\n\n")
+            time = ""
+            tmp = ""
+    elif time == "":
+        time = c[0]
+        tmp += (chunk + " ")
+    else:
+        tmp += (chunk + " ")
+
+
+
+f = open(output_vtt, "w")
+f.write(new)
+f.close()
